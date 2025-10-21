6a346410543fde73c1ea6308d3771d63b690c4c4
SITHARA-ABERATHNA <sitharaaberathna293@gmail.com>
2025-10-21 23:56:41 +0530

    Adding Location List

--- SmartCityRouteP.java
+++ SmartCityRouteP.java
@@ -48,6 +48,28 @@ class Graph {
     }
 }
 
+/*Use an AVL Tree or Linked List to store and display locations.*/
+
+
+class LocationList {
+    LinkedList<String> locations = new LinkedList<>();
+
+    public void add(String name) {
+        locations.add(name);
+    }
+
+    public void remove(String name) {
+        locations.remove(name);
+    }
+
+    public void display() {
+        for (String loc : locations) {
+            System.out.println("- " + loc);
+        }
+    }
+}
+
+
 public void bfsTraversal(String start) {
     Queue<String> queue = new LinkedList<>();
     Set<String> visited = new HashSet<>();
