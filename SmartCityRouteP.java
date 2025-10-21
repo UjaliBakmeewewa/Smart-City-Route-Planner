33ea5e5598457537bec17bf18a63eb7db0390845
Ru-janani <r7jananipeiris@gmail.com>
2025-10-21 23:38:56 +0530

    Adding the traversal method

--- SmartCityRouteP.java
+++ SmartCityRouteP.java
@@ -48,6 +48,26 @@ class Graph {
     }
 }
 
+public void bfsTraversal(String start) {
+    Queue<String> queue = new LinkedList<>();
+    Set<String> visited = new HashSet<>();
+
+    queue.add(start);
+    visited.add(start);
+
+    while (!queue.isEmpty()) {
+        String current = queue.poll();
+        System.out.print(current + " ");
+
+        for (String neighbor : adjList.get(current)) {
+            if (!visited.contains(neighbor)) {
+                visited.add(neighbor);
+                queue.add(neighbor);
+            }
+        }
+    }
+    System.out.println();
+}
 
 
 
